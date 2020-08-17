cd03bd5e558012db1ad4049b48357f5d9358e039
juan9794@gmail.com <juan9794@gmail.com>
2020-08-17 06:03:58 +0000

    Mumuki Export on 2020-08-17 06:03:58 +0000

--- 00075_Gran compañero de viaje/test.pl
+++ 00075_Gran compañero de viaje/test.pl
@@ -1,5 +1,8 @@
 test(juancho_es_gran_companiero_para_pepe, nondet):-
 	granCompanieroDeViaje(pepe, juancho).
+	
+test(pepe_no_es_gran_companiero_para_juancho, nondet):-
+	granCompanieroDeViaje(juancho, pepe).
  
 test(lucy_no_es_gran_companiera_para_pepe, fail):-
 	granCompanieroDeViaje(pepe, lucy).
