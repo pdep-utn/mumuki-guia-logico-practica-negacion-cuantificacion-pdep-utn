ea945e80b3fde223c96a5590a29c3ead90708597
juan9794@gmail.com <juan9794@gmail.com>
2020-08-19 22:28:18 +0000

    Mumuki Export on 2020-08-19 22:28:18 +0000

--- 00075_Gran compañero de viaje/extra.pl
+++ 00075_Gran compañero de viaje/extra.pl
@@ -1,7 +1,6 @@
 destino(pepe, alejandria).
 destino(pepe, jartum).
 destino(pepe, roma).
-destino(pepe, varna).
 destino(juancho, roma).
 destino(juancho, belgrado).
 destino(lucy, roma).
@@ -13,7 +12,6 @@ idioma(belgrado,serbio).
 idioma(roma, italiano).
 
 habla(pepe, bulgaro).
-habla(pepe,bulgaro).
 habla(pepe,italiano).
 habla(juancho,arabe).
 habla(juancho, italiano).
